d4aa7990f24cde499e6b8c2836c76e1f12782dc2
vishmi-28 <vishmigashree28@gmail.com>
2026-01-13 14:56:46 +0530

    Fix logo path case sensitivity

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("home");
